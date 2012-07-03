07f9f14f6b1ee821e96c5bfd3c1e1949841a0d27
Robin Edwards <robin.ge@gmail.com>
2012-07-03 22:57:14 +0100

    Fix stderr / stdout piping

--- job.c
+++ job.c
@@ -15,7 +15,6 @@ Job *Job_create(int id, char *command, int time_limit) {
     job->id = id;
     job->time_limit = time_limit;
     job->pid = -1;
-    job->stdin_fd = -1;
     job->stdout_fd = -1;
     job->stderr_fd = -1;
     return job;
@@ -24,12 +23,10 @@ Job *Job_create(int id, char *command, int time_limit) {
 void Job_destroy(Job *job) {
     assert(job != NULL);
     free(job->command);
-    if(job->stdin_fd > -1)
-        close(job->stdin_fd);
     if(job->stdout_fd > -1)
         close(job->stdout_fd);
     if(job->stderr_fd > -1)
-        close(job->stdin_fd);
+        close(job->stderr_fd);
     free(job);
 }
 
@@ -44,60 +41,40 @@ int Job_launch(Job *job) {
 
     // fd juggling adapted from here: http://jineshkj.wordpress.com/2006/12/22/how-to-capture-stdin-stdout-and-stderr-of-child-program/
 
-    int child_stdout_fd[2];
-    int child_stdin_fd[2];
-    int child_stderr_fd[2];
-    int old_stdin, old_stdout, old_stderr;
+    int child_stdout_fd[2], child_stderr_fd[2];
 
     // parent fds
-    pipe(child_stdin_fd);
-    pipe(child_stdout_fd);
-    pipe(child_stderr_fd);
-
-    // save current fds
-    old_stdin = dup(STDIN_FILENO);
-    old_stdout = dup(STDOUT_FILENO);
-    old_stderr = dup(STDERR_FILENO);
-
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-
-    // set read end of pipe as stdin
-    dup2(child_stdin_fd[0], STDIN_FILENO);
-
-    // set write end of pipes as stdout & stderr
-    dup2(child_stdout_fd[1], STDOUT_FILENO);
-    dup2(child_stderr_fd[1], STDERR_FILENO);
+    if(pipe(child_stdout_fd) == -1)
+        die("Couldn't setup pipe");
+    if(pipe(child_stderr_fd) == -1)
+        die("Couldn't setup pipe");
 
     int pid = fork();
     if (pid == 0) { // child
-        close(child_stdin_fd[0]);
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        // set write end of pipes as stdout & stderr
+        if(dup2(child_stdout_fd[1], STDOUT_FILENO) == -1)
+            die("Couldn't copy fd");
+        if(dup2(child_stderr_fd[1], STDERR_FILENO) == -1)
+            die("Couldn't copy fd");
+
         close(child_stdout_fd[0]);
         close(child_stderr_fd[0]);
-        close(child_stdin_fd[1]);
         close(child_stdout_fd[1]);
         close(child_stderr_fd[1]);
 
         printf("Child executing %s\n", job->command);
-        execv(job->command, 0);
-        printf("Child finished\n");
+        exit(execv(job->command, 0));
     }
     else if (pid > 0) { // parent
         job->pid = pid;
-        // restore old fds
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-        dup2(old_stdin, STDIN_FILENO);
-        dup2(old_stdout, STDOUT_FILENO);
-        dup2(old_stderr, STDERR_FILENO);
         // used by child
-        close(child_stdin_fd[0]);
         close(child_stdout_fd[1]);
         close(child_stderr_fd[1]);
         // store fds
-        job->stdin_fd = child_stdin_fd[1];
         job->stdout_fd = child_stdout_fd[0];
         job->stderr_fd = child_stderr_fd[0];
         printf("Job %d launched with pid %d\n", job->id, job->pid);
