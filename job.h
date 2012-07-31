e9d8044f9abf2614ee08704cdc7842c86b29a375
Robin Edwards <robin.ge@gmail.com>
2012-07-31 21:53:47 +0100

    Remove unused records

--- job.h
+++ job.h
@@ -2,9 +2,6 @@
 
 typedef struct {
     char *command;
-    char *std_output;
-    char *std_error;
-    int fd;
     int id;
     int pid;
     int exit_code;
@@ -15,12 +12,11 @@ typedef struct {
     struct event * stdout_ev;
     char *stderr_output;
     char *stdout_output;
-
 } Job;
 
 Job *Job_create(int id, char *command, int time_limit);
-void Job_setup_child_handler(void);
 void Job_destroy(Job *job);
+void Job_setup_child_handler(void);
 void Job_print(Job *job);
 int Job_launch(Job *job);
 int Job_setup_pipes(Job *job, struct event_base *base);
