e906b9a02d25ef4d780fb91f739d658fc37882d8
Robin Edwards <robin.ge@gmail.com>
2012-08-02 22:27:42 +0100

    Read port from argv

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "event_server.h"
 #include "job_manager.h"
+#include <stdlib.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+    int port = atoi(argv[1]);
     JobManager_create(10);
-    EventServer* s = EventServer_create(9090);
-    printf("Listening on port 9090\n");
+    printf("Listening on port %d\n", port);
+    EventServer* s = EventServer_create(port);
     EventServer_run(s);
     JobManager_destroy();
     return 0;
